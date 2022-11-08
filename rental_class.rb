422979fd50be20233cbeaab79010d5701fe8c2c5
lucabenaventew <cacachuuuluca@gmail.com>
2022-11-07 19:01:36 -0500

    Required changes completed

--- rental_class.rb
+++ rental_class.rb
@@ -2,10 +2,10 @@ class Rental
   attr_accessor :date, :book, :person
 
   def initialize(date, book, person)
-    @person = person
-    person.rentals << self
+    @date = date
     @book = book
     book.rentals << self
-    @date = date
+    @person = person
+    person.rentals << self
   end
 end
